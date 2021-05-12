bc4825ee7c8cd7a30d32466c42b4467eb9cee19f
andrey <andrey.nesterov@laposte.net>
2021-05-12 13:27:04 +0200

    db connection class fix

--- include/class/db.class.php
+++ include/class/db.class.php
@@ -4,13 +4,15 @@
  */
 class database
 {
-	private static $_host = "localhost";
-	private static $_user = "root";
-	private static $_mdp = "";
-	private static $_bdd = "joe_agenda";
+    private static $_host = "localhost";
+    private static $_user = "root";
+    private static $_mdp = "";
+    private static $_bdd = "joe_agenda";
 
-	public static $_conn;
+    public static $_conn;
 
-	public static function createConnexion(){
-		self::$_conn = new pdo("mysql:host=".self::$_host.";dbname=".self::$_bdd.";charset=UTF8", self::$_user, self::$_mdp);
-	}
+    public static function createConnexion()
+    {
+        self::$_conn = new pdo("mysql:host=".self::$_host.";dbname=".self::$_bdd.";charset=UTF8", self::$_user, self::$_mdp);
+    }
+}
